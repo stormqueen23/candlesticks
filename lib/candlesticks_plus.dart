a527f476459df8c26fdeda5405aee28251b5fd1f
stormqueen <melanie@siekmoeller.com>
2022-05-26 10:50:06 +0200

    export candleStyle too

--- lib/candlesticks_plus.dart
+++ lib/candlesticks_plus.dart
@@ -2,5 +2,6 @@ library candlesticks_plus;
 
 export 'src/main.dart';
 export 'src/models/candle.dart';
+export 'src/models/candle_style.dart';
 export 'src/widgets/toolbar_action.dart';
 export 'src/theme/theme_data.dart';
